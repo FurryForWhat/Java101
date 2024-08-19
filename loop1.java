4622c4e4dffb96dfda6dab571e28403855e9deed
Furry33 <mathet426@gmail.com>
2024-08-19 22:13:20 +0700

    Initial commit

---
+++ loop1.java
@@ -0,0 +1,33 @@
+package looping;
+
+import java.util.Scanner;
+
+public class loop1 {
+    
+    public static void main(String[] args){
+    //P1     int total =0;
+    //     for(int i = 1; i <= 10;i++){
+    //         total += i;
+    //     }
+    //     System.out.println  ("The sum of numbers from 1 to 10 is:"+total);
+    
+////////////////////////////////////////////////////////
+    //P2 Scanner scan = new Scanner(System.in);
+    // System.out.print("Enter num:");
+    // int myThing = scan.nextInt();
+    // int total = 1; 
+    // for(int i = 1 ; i <= myThing;i++){
+    //     total*= i;
+    // }
+    // System.out.println("The factorial of "+myThing+" is "+total);
+
+    ///////////////////////////////////////////////////
+    //P3 Scanner scan = new Scanner(System.in);
+    // System.out.println("Enter num");
+    // int myInt = scan.nextInt();
+    // System.out.println("Multiplication table for 7.");
+    // for(int i = 1; i < 10; i++){
+    //     System.out.println(myInt+" * "+i+" ="+ myInt*i);
+    // }
+}
+}
